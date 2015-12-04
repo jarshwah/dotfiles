f8ff33db45f6796d0169229b0cdbca00c42aa6d2
Josh Smeaton <josh.smeaton@gmail.com>
2015-12-04 11:34:23 +1100

    Add some new programs

--- symlink/brew.sh
+++ symlink/brew.sh
@@ -1,24 +1,37 @@
 # Install command-line tools using Homebrew
 # Usage: `./brew.sh`
 
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Make sure we’re using the latest Homebrew
 brew update
-brew upgrade
+brew upgrade --all
 
 brew tap homebrew/dupes
 brew tap homebrew/apache
 
 brew install --universal pcre
 
-brew install vim --override-system-vi
 brew install coreutils
-brew install findutils --default-names
-brew install gnu-sed --default-names
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+brew install moreutils
+brew install findutils --with-default-names
+brew install gnu-sed --with-default-names
 brew install bash
-brew install bash-completion
-
+brew tap homebrew/versions
+brew install bash-completion2
+brew install readline
 brew install wget --enable-iri
+brew install vim --override-system-vi
 brew install homebrew/dupes/grep
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
 brew install ack
 brew install autoconf
 brew install autojump
@@ -38,7 +51,6 @@ brew install pstree
 brew install python
 brew install mercurial
 brew install python3
-brew install readline
 brew install rename
 brew install ssh-copy-id
 brew install tmux
